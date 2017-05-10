d7b3aa498168e1a3a889f54da81386582a548f89
David Durkin <dldurks@gmail.com>
2017-05-10 00:07:22 -0400

    reverted

--- simple/galaga.py
+++ simple/galaga.py
@@ -71,34 +71,6 @@ class Player(pygame.sprite.Sprite):
 				self.state=0
 				self.image=self.player_ship[0]
 
-class EnemyManager(pygame.sprite.RenderUpdates):
-	def __init__(self):
-		pygame.sprite.RenderUpdates.__init__(self)
-		self.frames=0
-		self.transition_speed=5
-		self.transition_time=150/self.transition_speed
-		self.current_transition=0
-
-	def shoot(self,shotslist):
-		self.frames=random.randint(0,15)
-		if self.frames < len(self):
-			self.sprites()[self.frames].shoot(shotslist)
-
-	def update(self):
-		if self.current_transition<self.transition_time:
-			for e in self:
-				if(e.is_wily() == True):
-					e.update()
-				else:
-					e.update(self.transition_speed)
-			self.current_transition+=1
-		else:
-			for e in self:
-				if(e.is_wily() == True):
-					e.update()
-				else:
-					e.update(0)
-
 class WilyEnemy(pygame.sprite.Sprite):
 	enx=0
 	eny=30
@@ -106,8 +78,8 @@ class WilyEnemy(pygame.sprite.Sprite):
 	def __init__(self, parent):
 		self.parent=parent
 		pygame.sprite.Sprite.__init__(self)
-		self.enxspeed=10
-		self.enyspeed=10
+		self.enxspeed=random.randrange(-25,25)
+		self.enyspeed=random.randrange(-25,25)
 		self.en_xmax=WIN_RESX
 		self.en_xmin=0
 		self.en_Y_MAX=WIN_RESY-100
@@ -117,7 +89,7 @@ class WilyEnemy(pygame.sprite.Sprite):
 		self.image= self.wily_ship
 		self.rect = self.image.get_rect()
 		self.step = 0
-		self.threshold = 35
+		self.threshold = random.randrange(25,75)
 		self.explosions=[]
 		self.explosions.append(load_image('images/boom1.bmp'))
 		self.explosions.append(load_image('images/boom2.bmp'))
@@ -128,17 +100,22 @@ class WilyEnemy(pygame.sprite.Sprite):
 	def is_wily(self):
 		return True
 
-	def set_pos(self, x, y):
-		self.rect.move_ip(x,y)
+	def set_pos(self):
+		self.rect.move_ip(random.randrange(self.en_xmin,self.en_xmax),random.randrange(self.en_Y_MIN,self.en_Y_MAX-150))
 
-	def set_speed(self, speedx, speedy):
-		self.enxspeed=speedx
-		self.enyspeed=speedy
+	def set_speed(self, speed):
+		self.enspeed=speed
 
 	def get_range(self):
 		return self.en_xmin,self.en_xmax
 
 	def update(self):
+		if self.step >= self.threshold:
+			self.enxspeed = random.randrange(-15,15)
+			self.enyspeed = random.randrange(-15,15)
+			self.step = 0
+			self.threshold = random.randrange(25,75)
+		self.step += 1
 
 		if (self.rect.x <0) or (self.rect.x > self.en_xmax):
 			self.enxspeed *= -1
@@ -207,14 +184,11 @@ class Galaga:
 		self.player2.set_pos(400,550)
 		self.bullets1 = pygame.sprite.RenderUpdates()
 		self.bullets2 = pygame.sprite.RenderUpdates()
-		self.ebullets = pygame.sprite.RenderUpdates()
-		self.enemies = EnemyManager()
+		self.enemies = pygame.sprite.RenderUpdates()
 		self.bulletlist = []
 		self.enemylist = []
-		self.ebulletlist = []
 		self.gs = pygame.display.get_surface()
 		self.s = pygame.Surface((WIN_RESX,WIN_RESY))
-		self.set = 0
 
 		pygame.key.set_repeat(1,30)
 
@@ -234,50 +208,41 @@ class Galaga:
 				if event.key == 276 or event.key == 275 or event.key == 32:
 					self.sendData(event.key)
 
+		#check collisions
+		dead1=pygame.sprite.groupcollide(self.enemies, self.bullets1,0,0)
+		dead2=pygame.sprite.groupcollide(self.enemies, self.bullets2,0,0)
+		for enemy,bullet in dead1.iteritems():
+			self.bullets1.remove(bullet)
+			enemy.set_state(0)
+			#points.add_points(1)
+			self.player1.bullets += 5
+			##bullets.add_bullets(5)
+		for enemy,bullet in dead2.iteritems():
+			self.bullets2.remove(bullet)
+			enemy.set_state(0)
+			#points.add_points(1)
+			self.player2.bullets += 5
+			##bullets.add_bullets(5)
+
+		#draw players
+		self.screen.blit(self.background, self.bgrect)
+		self.screen.blit(self.player1.image, self.player1.rect)
+		self.screen.blit(self.player2.image, self.player2.rect)
+		#draw bullets
+		self.bullets1.clear(self.screen, self.background)
+		self.bullets2.clear(self.screen, self.background)
+		self.bulletlist+=self.bullets1.draw(self.screen)
+		self.bulletlist+=self.bullets2.draw(self.screen)
+		self.bullets1.update()
+		self.bullets2.update()
+		#draw enemies
 		try:
-			#check collisions
-			dead1=pygame.sprite.groupcollide(self.enemies, self.bullets1,0,0)
-			dead2=pygame.sprite.groupcollide(self.enemies, self.bullets2,0,0)
-			for enemy,bullet in dead1.iteritems():
-				i=0
-				for enemy1 in self.enemies:
-					if enemy1 == enemy:
-						print(i)
-					i+=1
-				self.bullets1.remove(bullet)
-				enemy.set_state(0)
-				#points.add_points(1)
-				self.player1.bullets += 5
-				##bullets.add_bullets(5)
-			for enemy,bullet in dead2.iteritems():
-				self.bullets2.remove(bullet)
-				enemy.set_state(0)
-				#points.add_points(1)
-				self.player2.bullets += 5
-				##bullets.add_bullets(5)
 
-			#draw players
-			self.screen.blit(self.background, self.bgrect)
-			self.screen.blit(self.player1.image, self.player1.rect)
-			self.screen.blit(self.player2.image, self.player2.rect)
-			#draw bullets
-			self.bullets1.clear(self.screen, self.background)
-			self.bullets2.clear(self.screen, self.background)
-			self.bulletlist+=self.bullets1.draw(self.screen)
-			self.bulletlist+=self.bullets2.draw(self.screen)
-
-			self.bullets1.update()
-			self.bullets2.update()
-			#draw enemies
 			if not self.enemies:
-				self.sendData(0)
-				if self.set == 1:
-					for _ in range(5):
-						wily = WilyEnemy(self.enemies)
-						wily.set_pos(10,10)
-						wily.set_speed(self.newx, self.newy)
-						self.enemies.add(wily)
-					self.set = 0
+				for _ in range(5):
+					wily = WilyEnemy(self.enemies)
+					wily.set_pos()
+					self.enemies.add(wily)
 			self.enemies.clear(self.screen, self.background)
 			self.enemylist+=self.enemies.draw(self.screen)
 
@@ -294,8 +259,6 @@ class Galaga:
 			self.outgoingConn.transport.write("1:" + str(keyNum))
 		else:
 			self.outgoingConn.transport.write("2:" + str(keyNum))
-		if keyNum == 0:
-			self.outgoingConn.transport.write("3:1")
 
 	def transferConnectionObject(self, obj):
 		self.outgoingConn = obj
@@ -313,10 +276,6 @@ class Galaga:
 			self.player1.shoot(self.bullets1, self.player1.rect.centerx, self.player1.rect.top)
 		elif data['p2Shot'] == '1':
 			self.player2.shoot(self.bullets2, self.player2.rect.centerx, self.player2.rect.top)
-		if data['speedSet'] == '1':
-			self.newx = int(data['speedx'])
-			self.newy = int(data['speedy'])
-			self.set = 1
 
 if __name__ == "__main__":
 	game=Galaga(1)
--- simple/galagaServer.py
+++ simple/galagaServer.py
@@ -8,7 +8,6 @@ from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
 from twisted.internet.task import LoopingCall
-import random
 import json
 # import globalvars
 
@@ -22,6 +21,22 @@ PLAYER2_HOST = ""
 # Global deferred queue, handles input from both players
 dq = DeferredQueue()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class GameState:
     #def main(self):
     def __init__(self):
@@ -31,9 +46,6 @@ class GameState:
         self.p2SHIP_l = '0'
         self.p1Shot = '0'
         self.p2Shot = '0'
-        self.randx = 0
-        self.randy = 0
-        self.set = 0
 
     def getPlayer1_Connection(self, p1CONN):
         self.player1_Conn = p1CONN
@@ -49,9 +61,6 @@ class GameState:
         self.p2SHIP_r = '0'
         self.p1Shot = '0'
         self.p2Shot ='0'
-        self.randx = 0
-        self.randy = 0
-        self.set = 0
 
         dataList = data.split(":")
         if dataList[1] == '-1':
@@ -74,18 +83,12 @@ class GameState:
             elif dataList[1] == '32':
                 self.p2Shot = '1'
 
-        elif dataList[0] == '3':
-            if dataList[1] == '1':
-                self.randx = random.randrange(-15,15)
-                self.randy = random.randrange(-15,15)
-                self.set = 1
-            else:
-                self.randx = 0
-                self.randy = 0
-                self.set = 0
-
         else:
             print "Error: unexpected data sent from Player"
+
+        # self.applyBoosts() # if either player ran over a boost, apply it
+        # self.checkWinner() # check at each tick if a player has won
+
 
         return_string = json.dumps({
                                     'p1Ship_l':self.p1SHIP_l,
@@ -94,15 +97,29 @@ class GameState:
                                     'p2Ship_r':self.p2SHIP_r,
                                     'p1Shot':self.p1Shot,
                                     'p2Shot':self.p2Shot,
-                                    'speedx':str(self.randx),
-                                    'speedy':str(self.randy),
-                                    'speedSet':str(self.set),
                                     })
         self.player1_Conn.sendData(return_string)
         self.player2_Conn.sendData(return_string)
         dq.get().addCallback(self.decode_data) # after decode data, reattach callback
 
+
+
+
+
+
+
+
+
+
+
+
 gs = GameState()
+
+
+
+
+
+
 
 class Player1_Connection(Protocol):
 
